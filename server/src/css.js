9e51beb1b8e9515c50a2c39d34caa692ebd057da
Oliver Joseph Ash <oliverjash@gmail.com>
2016-05-22 11:18:53 +0100

    Fix margins

--- server/src/css.js
+++ server/src/css.js
@@ -19,11 +19,24 @@ body {
     justify-content: center;
 }
 
+.element-group.image:not(:last-child),
+.element-group.text:not(:last-child) {
+    margin-bottom: 1rem;
+}
+
 @media (max-width: 767px) {
     .element-group.image-square-pair {
         width: calc(100% + 2rem);
         flex-direction: column;
         align-items: center;
+    }
+
+    .element-group.image-square-pair .image-element {
+        margin-bottom: 1rem;
+    }
+
+    .element-group.image-square-pair:last-child .image-element:last-child {
+        margin-bottom: 0;
     }
 }
 
@@ -39,13 +52,15 @@ body {
         padding-left: 1rem;
     }
 
+    .element-group.image-square-pair:not(:last-child) {
+        margin-bottom: 1rem;
+    }
 }
 
 .image-element {
     width: 100%;
     display: flex;
     justify-content: center;
-    margin-bottom: 1rem;
 }
 
 .image-element-inner-1 {
@@ -74,5 +89,23 @@ body {
 /* Already fixed and black, as according to spec */
 .image-element-inner-2 img:-moz-full-screen {
     object-fit: contain;
+}
+
+h1, h2, h3, h4, h5, h6, p {
+    margin-top: 0;
+    margin-bottom: 0;
+}
+
+ul {
+    list-style: none;
+    padding-left: 0;
+}
+
+h1, h2, h3, h4, h5, h6, p, .article-header, .block {
+    margin-bottom: 1rem;
+}
+
+p:last-child {
+    margin-bottom: 0;
 }
 `;
