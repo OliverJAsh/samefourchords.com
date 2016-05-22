c56d4686488a73c1935e6dc5257ba38875764eb0
Oliver Joseph Ash <oliverjash@gmail.com>
2016-05-22 11:19:21 +0100

    Move blocks into fragment, move post model into partial

---
+++ server/src/fragments/blocks.ts
@@ -0,0 +1,64 @@
+import { h } from 'virtual-dom';
+
+import { Model, ImageSquarePairGroup, ImageGroup, TextGroup, ImageElement } from '../post-model';
+
+const renderImage = (element: ImageElement) => (
+    h('.image-element', {
+        style: { maxWidth: `calc(${element.widthAsProportionOfHeight} * 100vh)` }
+    }, [
+        h('.image-element-inner-1', {
+            style: { maxWidth: `${element.masterWidth}px` }
+        }, [
+            h('.image-element-inner-2', {
+                style: { paddingBottom: `${element.heightAsProportionOfWidth * 100}%` }
+            }, [
+                h('picture', [
+                    {
+                        media: '(min-resolution: 2dppx)',
+                        srcset: element.highDprSrcset
+                    },
+                    {
+                        media: '',
+                        srcset: element.srcset
+                    }
+                ].map(({ media, srcset }) => (
+                    h('source', {
+                        sizes: [
+                            `(min-aspect-ratio: ${element.aspectRatio.join('/')}) ${element.widthAsProportionOfHeight * 100}vh`,
+                            '100vw'
+                        ].join(', '),
+                        media,
+                        srcset: srcset
+                            .map(size => `${size.file} ${size.width}w`)
+                            .join(', ')
+                    }, [])
+                )).concat(
+                    h('img', { src: element.firstSize.file }, [])
+                ))
+            ])
+        ])
+    ])
+);
+
+export default (model: Model) => (
+    h('.blocks', model.blocks.map(block => (
+        h('.block', [
+            block.title ? h('h5', block.title) : null,
+            h('.element-groups', (
+                block.elementGroups.map(elementGroup => {
+                    return h('.element-group', { className: elementGroup.type }, (
+                        (() => {
+                            if (elementGroup instanceof ImageSquarePairGroup) {
+                                return elementGroup.elements.map(renderImage)
+                            } else if (elementGroup instanceof ImageGroup) {
+                                return [ renderImage(elementGroup.element) ];
+                            } else if (elementGroup instanceof TextGroup) {
+                                return [ h('.text-element', { innerHTML: elementGroup.element.body }, []) ];
+                            }
+                        })()
+                    ));
+                })
+            ))
+        ])
+    )))
+);
---
+++ server/src/post-model.ts
@@ -0,0 +1,134 @@
+import { h } from 'virtual-dom';
+import * as URL from 'url';
+import { range } from 'lodash';
+import dateFormat = require('dateformat');
+
+import { Post, PostImageElement, PostTextElement } from './models';
+
+export interface Model {
+    title: string;
+    href: string;
+    date: string;
+    blocks: Array<{
+        title: string;
+        elementGroups: Array<Group>
+    }>
+}
+
+abstract class Group {
+    constructor(public type: string) {}
+}
+
+export class ImageGroup extends Group {
+    constructor(public element: ImageElement) { super('image') }
+}
+
+export class TextGroup extends Group {
+    constructor(public element: TextElement) { super('text') }
+}
+
+export class ImageSquarePairGroup extends Group {
+    constructor(public elements: [ ImageElement, ImageElement ]) { super('image-square-pair') }
+}
+
+interface ImageElementSize {
+    file: string;
+    width: number;
+}
+
+abstract class Element {
+    constructor() {}
+}
+
+class TextElement extends Element {
+    constructor(public body: string) { super() }
+}
+
+export class ImageElement extends Element {
+    constructor(
+        public heightAsProportionOfWidth: number,
+        public widthAsProportionOfHeight: number,
+        public masterWidth: number,
+        public aspectRatio: [number, number],
+        public srcset: Array<ImageElementSize>,
+        public highDprSrcset: Array<ImageElementSize>,
+        public firstSize: ImageElementSize
+    ) { super() }
+}
+
+const imgixOrigin = 'https://samefourchords-com-images.imgix.net';
+const bucketName = 'samefourchords.com-images';
+
+// TODO: Class
+const isPostImageElement = (element: PostTextElement | PostImageElement): element is PostImageElement => element.type === 'image';
+const isPostTextElement = (element: PostTextElement | PostImageElement): element is PostTextElement => element.type === 'text';
+
+const getGcd = (a: number, b: number): number => b ? getGcd(b, a % b) : a;
+const simplify = (numerator: number, denominator: number): [number, number] => {
+  const gcd = getGcd(numerator,denominator);
+  return [numerator/gcd, denominator/gcd];
+};
+
+export const createModel = (post: Post): Model => {
+    return {
+        title: post.title,
+        href: post.href,
+        date: dateFormat(post.date, 'd mmmm yyyy'),
+        blocks: post.blocks.map(block => ({
+            title: block.title,
+            elementGroups: (
+                block.elements
+                    .map((element): Element => {
+                        if (isPostImageElement(element)) {
+                            const bucketPath = URL.parse(element.master.file).path.replace(new RegExp(`^/${bucketName}`), '');
+                            const heightAsProportionOfWidth = (element.master.height / element.master.width);
+                            const widthAsProportionOfHeight = (element.master.width / element.master.height);
+                            const createWidths = (dpr: number) => range(320 * dpr, element.master.width, 150 * dpr);
+                            const srcset = createWidths(1).map((width): ImageElementSize => ({
+                                file: `${imgixOrigin}${bucketPath}?auto=format%2Ccompress&w=${width}`,
+                                width
+                            }));
+                            const highDprSrcset = createWidths(2).map((width): ImageElementSize => ({
+                                file: `${imgixOrigin}${bucketPath}?auto=format&w=${width}&q=25&usm=20`,
+                                width
+                            }));
+
+                            return new ImageElement(
+                                heightAsProportionOfWidth,
+                                widthAsProportionOfHeight,
+                                // TODO: Option?
+                                element.master.width,
+                                simplify(element.master.width, element.master.height),
+                                srcset,
+                                highDprSrcset,
+                                // TODO: Option?
+                                srcset[0]
+                            );
+                        }
+                        else if (isPostTextElement(element)) {
+                            return new TextElement(element.body);
+                        }
+                    })
+                    .map((element): Group => {
+                        if (element instanceof ImageElement) {
+                            return new ImageGroup(element);
+                        } else if (element instanceof TextElement) {
+                            return new TextGroup(element);
+                        }
+                    })
+                    .reduceRight((acc, group) => {
+                        const prevGroup = acc[0];
+                        const isGroupSquareImage = (group: Group): group is ImageGroup =>
+                            group instanceof ImageGroup && group.element.widthAsProportionOfHeight === 1;
+
+                        if (isGroupSquareImage(group) && prevGroup && isGroupSquareImage(prevGroup)) {
+                            acc[0] = new ImageSquarePairGroup([ group.element, prevGroup.element ])
+                        } else {
+                            acc.unshift(group);
+                        }
+                        return acc;
+                    }, <Group[]>[])
+            )
+        }))
+    }
+};
